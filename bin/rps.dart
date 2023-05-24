212126944c4fb34bb242c8e7b409ab87fba5fd86
gonuit <kamilklyta341@gmail.com>
2023-05-24 19:41:36 +0200

    feat: add rps --upgrade option

--- bin/rps.dart
+++ bin/rps.dart
@@ -5,25 +5,54 @@ import 'package:rps/pubspec.dart';
 import 'package:rps/rps.dart' as rps;
 import 'package:rps/rps_exception.dart';
 import 'package:rps/utils.dart';
+import 'package:rps/bindings/execute.dart' as bindings;
 
 void main(List<String> args) async {
   try {
-    if (args.length == 1 && args.first.trim() == '--version') {
+    if (args.length == 1 && args.first.trim() == '--version' ||
+        args.first.trim() == '-v') {
       final ver = await rps.getPackageVersion();
-      stdout.writeln('rps version: ${rps.applyBold(ver)}');
+      stdout
+        ..writeln()
+        ..writeln('📝 rps version: ${rps.applyBold(ver)}')
+        ..writeln();
       exit(0);
-    } else if (args.length == 1 && args.first.trim() == '--help') {
+    } else if (args.length == 1 && args.first.trim() == '--help' ||
+        args.first.trim() == '-h') {
       final ver = await rps.getPackageVersion();
       stdout.writeln(
         '${applyBold('Run Pubspec Script')} (${applyBoldGreen('rps')}) ${rps.applyBold("v$ver")}\n\n'
         '${applyBold('Options')}:\n'
-        '  --version - prints version.\n'
-        '  --help    - prints help.\n'
+        '  -v, --version - prints version.\n'
+        '  -h, --help    - prints help.\n'
+        '  -u, --upgrade - upgrades rps package.\n'
         '${applyBold('Commands')}:\n'
-        '  run <pubspec command> <command options> - runs a script from pubspec.yaml file.\n',
+        '  run <pubspec script> [arguments] - runs a script from pubspec.yaml file.\n',
       );
       await stdout.flush();
       exit(0);
+    } else if (args.length == 1 && args.first.trim() == '--upgrade' ||
+        args.first.trim() == '-u') {
+      const command = 'dart pub global activate rps';
+      stdout.write(
+        '${applyBoldBlue('\n⏳ Upgrading rps package...')}\n\n'
+        '${applyBoldGreen(r'$')} ${applyBold(command)}\n\n',
+      );
+
+      final exitCode = await bindings.execute(command);
+      if (exitCode > 0) {
+        throw RpsException(
+          'Failed to update the rps package. '
+          'Command ended with a non zero exit code.',
+        );
+      } else {
+        stdout
+          ..writeln()
+          ..writeln(applyBoldGreen('✓ rps updated successfully!'))
+          ..writeln();
+        await stdout.flush();
+      }
+      exit(exitCode);
     } else {
       final pubspec = await Pubspec.load(Directory.current);
       final runCommand = RunCommand(pubspec);
