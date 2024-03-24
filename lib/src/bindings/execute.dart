65f34f0e337c465e20c0d1afab759aeb78fc683c
gonuit <kamilklyta341@gmai.com>
2024-03-24 09:44:55 +0100

    chore: improve abi handling

--- lib/src/bindings/execute.dart
+++ lib/src/bindings/execute.dart
@@ -1,31 +1,53 @@
 import 'dart:ffi';
-import 'dart:io';
 import 'dart:isolate';
 
 import 'package:ffi/ffi.dart';
 import 'package:path/path.dart' as path;
+import 'package:rps/rps.dart';
 
 typedef ExecuteNative = Int32 Function(Pointer<Utf8> command);
 typedef Execute = int Function(Pointer<Utf8> command);
 typedef ExecuteFunction = Future<int> Function(String command);
 
-Future<int> execute(String command) async {
+Future<int> execute(
+  String command, {
+  bool verbose = false,
+  StringSink? out,
+}) async {
+  final bindings = <Abi, String>{
+    Abi.windowsX64: 'rps_x64.dll',
+    Abi.linuxX64: 'librps_x64.so',
+    Abi.linuxArm64: 'librps_aarch64.so',
+    Abi.macosX64: 'librps.dylib',
+    Abi.macosArm64: 'librps.dylib',
+  };
+
   const rootLibrary = 'package:rps/rps.dart';
   final uri = await Isolate.resolvePackageUri(Uri.parse(rootLibrary));
   if (uri == null) {
-    print('Cannot load the library.');
-    exit(1);
+    throw RpsException('Cannot load the library.');
+  }
+
+  final platform = Abi.current();
+  if (verbose) {
+    out?.writeln("Running on platform: $platform");
+  }
+
+  String? libraryName = bindings[platform];
+
+  if (verbose) {
+    out?.writeln("Dynamic library file selected: $libraryName");
+  }
+
+  if (libraryName == null) {
+    throw RpsException('Current platform ($platform) is currently not supported.');
   }
 
   final root = path.fromUri(uri.resolve(path.join('..', 'native')).path);
-  // Open the dynamic library
-  late String libraryPath;
-  if (Platform.isMacOS) {
-    libraryPath = path.join(root, 'librps.dylib');
-  } else if (Platform.isWindows) {
-    libraryPath = path.join(root, 'rps.dll');
-  } else {
-    libraryPath = path.join(root, 'librps.so');
+  final libraryPath = path.join(root, libraryName);
+
+  if (verbose) {
+    out?.writeln("Dynamic library path: $libraryPath");
   }
 
   final dylib = DynamicLibrary.open(libraryPath);
